1c8f4a0926c6f38319e60d7258e6f59147b31feb
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2025-10-23 13:54:27 +0200

    Add JavaDoc for Event classes

--- api/src/main/java/net/potatocloud/api/event/EventListener.java
+++ api/src/main/java/net/potatocloud/api/event/EventListener.java
@@ -2,6 +2,11 @@ package net.potatocloud.api.event;
 
 public interface EventListener<T extends Event> {
 
+    /**
+     * Called then the event was fired
+     *
+     * @param event the event
+     */
     void onEvent(T event);
 
 }
--- api/src/main/java/net/potatocloud/api/event/EventManager.java
+++ api/src/main/java/net/potatocloud/api/event/EventManager.java
@@ -2,10 +2,29 @@ package net.potatocloud.api.event;
 
 public interface EventManager {
 
+    /**
+     * Registers a listener for the given event class.
+     *
+     * @param eventClass the class of the event to listen for
+     * @param listener   the listener to handle the event
+     * @param <T>        the type of the event
+     */
     <T extends Event> void on(Class<T> eventClass, EventListener<T> listener);
 
+    /**
+     * Calls an event locally on this api instance.
+     *
+     * @param event the event to call
+     * @param <T> the type of the event
+     */
     <T extends Event> void callLocal(T event);
 
+    /**
+     * Calls an event globally on all api instances.
+     *
+     * @param event the event to call
+     * @param <T> the type of the event
+     */
     <T extends Event> void call(T event);
 
 }
