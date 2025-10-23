7cd83c5a63c59394f727767612ca950ed08231d3
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2025-10-23 13:25:16 +0200

    Modified ServiceGroup java doc

--- api/src/main/java/net/potatocloud/api/group/ServiceGroup.java
+++ api/src/main/java/net/potatocloud/api/group/ServiceGroup.java
@@ -22,14 +22,14 @@ public interface ServiceGroup extends PropertyHolder {
     /**
      * Gets the name of the platform of the group.
      *
-     * @return the name of the platform
+     * @return the name of the platform of the group
      */
     String getPlatformName();
 
     /**
      * Gets the platform of the group as an object.
      *
-     * @return the platform
+     * @return the platform of the group
      */
     default Platform getPlatform() {
         return CloudAPI.getInstance().getPlatformManager().getPlatform(getPlatformName());
@@ -38,14 +38,14 @@ public interface ServiceGroup extends PropertyHolder {
     /**
      * Gets the name of the platform version of the group.
      *
-     * @return the name of the platform version
+     * @return the name of the platform version of the group
      */
     String getPlatformVersionName();
 
     /**
      * Gets the platform version of the group as an object.
      *
-     * @return the platform version
+     * @return the platform version of the group
      */
     default PlatformVersion getPlatformVersion() {
         return getPlatform().getVersion(getPlatformVersionName());
@@ -54,42 +54,42 @@ public interface ServiceGroup extends PropertyHolder {
     /**
      * Gets the list of service templates of the group.
      *
-     * @return the list of service templates
+     * @return the list of service templates of the group
      */
     List<String> getServiceTemplates();
 
     /**
      * Gets the minimum online count of the group.
      *
-     * @return the minimum online count
+     * @return the minimum online count of the group
      */
     int getMinOnlineCount();
 
     /**
      * Sets the minimum online count of the group.
      *
-     * @param minOnlineCount the minimum online count
+     * @param minOnlineCount the minimum online count of the group
      */
     void setMinOnlineCount(int minOnlineCount);
 
     /**
      * Gets the maximum online count of the group.
      *
-     * @return the maximum online count
+     * @return the maximum online count of the group
      */
     int getMaxOnlineCount();
 
     /**
      * Sets the maximum online count of the group.
      *
-     * @param maxOnlineCount the maximum online count
+     * @param maxOnlineCount the maximum online count of the group
      */
     void setMaxOnlineCount(int maxOnlineCount);
 
     /**
      * Gets the online players of the group.
      *
-     * @return the online players
+     * @return the online players of the group
      */
     default Set<CloudPlayer> getOnlinePlayers() {
         return CloudAPI.getInstance().getPlayerManager().getOnlinePlayersByGroup(this);
@@ -98,7 +98,7 @@ public interface ServiceGroup extends PropertyHolder {
     /**
      * Gets the online player count of the group.
      *
-     * @return the online player count
+     * @return the online player count of the group
      */
     default int getOnlinePlayerCount() {
         return getOnlinePlayers().size();
@@ -107,128 +107,128 @@ public interface ServiceGroup extends PropertyHolder {
     /**
      * Gets the maximum players of the group.
      *
-     * @return the maximum players
+     * @return the maximum players of the group
      */
     int getMaxPlayers();
 
     /**
      * Sets the maximum players of the group.
      *
-     * @param maxPlayers the maximum players
+     * @param maxPlayers the maximum players of the group
      */
     void setMaxPlayers(int maxPlayers);
 
     /**
      * Gets the maximum memory of the group.
      *
-     * @return the maximum memory
+     * @return the maximum memory of the group in MB
      */
     int getMaxMemory();
 
     /**
      * Sets the maximum memory of the group.
      *
-     * @param maxMemory the maximum memory
+     * @param maxMemory the maximum memory of the group in MB
      */
     void setMaxMemory(int maxMemory);
 
     /**
-     * Gets the fallback status of the group.
+     * Gets whether the group is a fallback.
      *
-     * @return the fallback status
+     * @return {@code true} if the group is a fallback, otherwise {@code false}
      */
     boolean isFallback();
 
     /**
-     * Sets the fallback status of the group.
+     * Sets whether the group is a fallback.
      *
-     * @param fallback the fallback status
+     * @param fallback {@code true} to make the group a fallback, otherwise {@code false}
      */
     void setFallback(boolean fallback);
 
     /**
-     * Gets the static status of the group.
+     * Gets whether the group is static.
      *
-     * @return the static status
+     * @return {@code true} if the group is static, otherwise {@code false}
      */
     boolean isStatic();
 
     /**
      * Gets the start priority of the group.
      *
-     * @return the start priority
+     * @return the start priority of the group
      */
     int getStartPriority();
 
     /**
      * Sets the start priority of the group.
      *
-     * @param startPriority the start priority
+     * @param startPriority the start priority of the group
      */
     void setStartPriority(int startPriority);
 
     /**
      * Gets the start percentage of the group.
      *
-     * @return the start percentage
+     * @return the start percentage of the group
      */
     int getStartPercentage();
 
     /**
      * Sets the start percentage of the group.
      *
-     * @param startPercentage the start percentage
+     * @param startPercentage the start percentage of the group
      */
     void setStartPercentage(int startPercentage);
 
     /**
-     * Gets the java command used to start services of the group.
+     * Gets the Java command used to start services of the group.
      *
-     * @return the java command
+     * @return the Java command of the group
      */
     String getJavaCommand();
 
     /**
      * Gets the custom jvm flags of the group.
      *
-     * @return the custom jvm flags
+     * @return the custom jvm flags of the group
      */
     List<String> getCustomJvmFlags();
 
     /**
      * Adds a custom jvm flag to the group.
      *
-     * @param flag the custom jvm flag
+     * @param flag the custom jvm flag to add
      */
     void addCustomJvmFlag(String flag);
 
     /**
      * Adds a service template to the group.
      *
-     * @param template the service template
+     * @param template the service template to add
      */
     void addServiceTemplate(String template);
 
     /**
      * Removes a service template from the group.
      *
-     * @param template the service template
+     * @param template the service template to remove
      */
     void removeServiceTemplate(String template);
 
     /**
      * Gets all services of the group.
      *
-     * @return the list of services
+     * @return the list of all services of the group
      */
     default List<Service> getAllServices() {
         return CloudAPI.getInstance().getServiceManager().getAllServices(getName());
     }
 
     /**
-     * Gets the online services of the group.
+     * Gets all online services of the group.
      *
-     * @return the list of online services
+     * @return the list of all online services of the group
      */
     default List<Service> getOnlineServices() {
         return CloudAPI.getInstance().getServiceManager().getOnlineServices(getName());
@@ -237,7 +237,7 @@ public interface ServiceGroup extends PropertyHolder {
     /**
      * Gets the online service count of the group.
      *
-     * @return the online service count
+     * @return the online service count of the group
      */
     default int getOnlineServiceCount() {
         return getOnlineServices().size();
