e1b740bf1b798501745f17c9ad39d0563d357f9c
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2025-12-09 14:31:29 +0100

    Add code comments to DefaultProperties

--- api/src/main/java/net/potatocloud/api/property/DefaultProperties.java
+++ api/src/main/java/net/potatocloud/api/property/DefaultProperties.java
@@ -4,8 +4,14 @@ import java.util.Set;
 
 public class DefaultProperties {
 
+    // Shows the current game state of the service
+    // When the game state changes to "INGAME", the cloud automatically starts a new service
     public static final Property<String> GAME_STATE = Property.ofString("gameState", "LOBBY");
+
+    // If true, the service uses Velocitys modern forwarding
     public static final Property<Boolean> VELOCITY_MODERN_FORWARDING = Property.ofBoolean("velocityModernForwarding", false);
+
+    // If true, the cloud always replaces the velocity forwarding secret with its own
     public static final Property<Boolean> ALWAYS_OVERRIDE_FORWARDING_SECRET = Property.ofBoolean("alwaysOverrideForwardingSecret", true);
 
     public static Set<Property<?>> asSet() {
